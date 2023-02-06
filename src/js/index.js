0280ce1837a6ec5f5f6debe615fac9189d2fb189
Arthur <blandin.arthur@gmail.com>
2023-02-06 23:54:40 +0000

    Beginning of navbar and darkmode button

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,27 @@
 var colorChange = document.getElementById("darkmode");
 var text = document.getElementsByTagName("p");
-
 colorChange.addEventListener('click', function(){
+    console.log("JS");
+    colorChange.style.transition="background-color 1s";
+
+    if (colorChange.classList.contains("dark")){
+        colorChange.classList.remove('dark', 'darkcolorfont'); 
+        colorChange.classList.add('light', 'lightcolorfont');
+        colorChange.innerHTML='<img src="https://www.svgrepo.com/download/79251/crescent-moon.svg" alt="">'; 
+    } else {
+        colorChange.classList.remove('light', 'lightcolorfont'); 
+        colorChange.classList.add('dark', 'darkcolorfont');
+        colorChange.innerHTML='<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Sun_white_icon.svg/1200px-Sun_white_icon.svg.png" alt="">'; 
+    }
+
     for (let i = 0; i < text.length; i++) {
         text[i].style.transition="color 1s"
-        colorChange.style.transition="background-color 1s";
         if (text[i].classList.contains("dark")){
         text[i].classList.remove('dark', 'darkcolortext');
-        colorChange.classList.remove('dark', 'darkcolorfont'); 
         text.item(i).classList.add('light', 'lightcolortext');
-        colorChange.classList.add('light', 'lightcolorfont');
-        colorChange.innerHTML="Light Mode"; 
         } else {
             text[i].classList.remove('light', 'lightcolortext');
-            colorChange.classList.remove('light', 'lightcolorfont'); 
             text.item(i).classList.add('dark', 'darkcolortext');    
-            colorChange.classList.add('dark', 'darkcolorfont');
-            colorChange.innerHTML="Dark Mode"; 
      
         }
       }
