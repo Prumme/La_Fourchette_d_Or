eecf594529ffc2c913851fc375b9fa9ccf423784
BLANDIN Arthur <blandin.arthur20@gmail.com>
2023-02-20 11:59:16 +0000

    Add Navbar burger menu + add close button on navbar mobile open

--- src/js/header.js
+++ src/js/header.js
@@ -2,3 +2,8 @@ function openNav() {
     document.getElementById("nav-mobile").className += " nav-mobile-open";
 
 }
+
+function closeNav() {
+    document.getElementById("nav-mobile").classList.remove("nav-mobile-open");
+
+}
